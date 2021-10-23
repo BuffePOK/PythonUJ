a418b46306c17976be17d76897aecd58e1587dc2
BuffePOK <DimaDichenko@gmail.com>
2021-10-23 17:04:30 +0200

    Revert "Zestaw01"
    This reverts commit a5f312b1df3db965fe2852d43af1baac970c0ad7.

--- Zestaw02/2.10.py
+++
@@ -1,6 +0,0 @@
-a = "Mamy dany napis wielowierszowy line. \
-Podać sposób obliczenia liczby wyrazów w napisie. \
-Przez wyraz rozumiemy ciąg \"czarnych\" znaków, \
-oddzielony od innych wyrazów białymi znakami (spacja, tabulacja, newline)."
-
-print(len(a.split()))
